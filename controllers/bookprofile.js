6cf3f6d7984d90f68f73a05cb08f444c1a6f81d4
dardangjoka <dardan.gjoka@gmail.com>
2023-04-04 21:35:41 +0200

    Filtrimi ka perfundu

--- controllers/bookprofile.js
+++ controllers/bookprofile.js
@@ -1,10 +1,10 @@
 const dbBookProfile={titulli:"Beni ecen vetem", autori:"Fan Noli", img:"/img/img1.jpg"};
-const dbBooks=[ {titulli:"Beni ecen nuk vetem", autori:"Fan Noli", img:"/img/img1.jpg"}, 
-                {titulli:"Beni nashta ecen  vetem", autori:"Fan Noli", img:"/img/img1.jpg"},
-                {titulli:"Beni munde te ecen vetem", autori:"Fan Noli", img:"/img/img1.jpg"},
-                {titulli:"Beni nuk ecen vetem", autori:"Fan Noli", img:"/img/img1.jpg"},
-                {titulli:"Beni nuk eshte fukara", autori:"Fan Noli", img:"/img/img1.jpg"},
-                {titulli:"Beni ecen vetem", autori:"Fan Noli", img:"/img/img1.jpg"},
+const dbBooks=[ {titulli:"Beni ecen nuk vetem", autori:"Fan Noli", img:"/img/img1.jpg", zhanri:"femij"}, 
+                {titulli:"Beni nashta ecen  vetem", autori:"Fan Noli", img:"/img/img1.jpg", zhanri:"femij"},
+                {titulli:"Beni munde te ecen vetem", autori:"Fan Noli", img:"/img/img1.jpg", zhanri:"komedi"},
+                {titulli:"Beni nuk ecen vetem", autori:"Fan Noli", img:"/img/img1.jpg", zhanri:"shkence"},
+                {titulli:"Beni nuk eshte fukara", autori:"Fan Noli", img:"/img/img1.jpg", zhanri:"romantik"},
+                {titulli:"Beni ecen vetem", autori:"Fan Noli", img:"/img/img1.jpg", zhanri:"romantik"},
             ];
 
 exports.getBookProfile=(req, res)=>{
@@ -15,7 +15,13 @@ exports.getBookProfile=(req, res)=>{
 
 exports.getAllBooks=(req, res)=>{
     const zhanri=req.query;
-    if(!zhanri.zhanri)
+    if(!(Object.keys(zhanri).length === 0) && zhanri.zhanri!=="clear"){
+        console.log(zhanri)
+        const filterdBooks=dbBooks.filter((c)=>{
+            return zhanri.zhanri===c.zhanri;
+        });
+        return res.render("librat/index",{titulliIfaqes:"Librat me zhaner:"+zhanri.zhanri, librat: filterdBooks});
+    }
     res.render("librat/index",{titulliIfaqes:"Te gjitha librat", librat: dbBooks})
 }
 
