10770b1f9756e49f80931a51a1f832baf60365e3
aaaBUTCHER <aa61491@ubt-uni.net>
2023-04-05 23:33:08 +0100

    u shtu profili userit

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -1,8 +1,9 @@
 const express=require("express");
 const router=express.Router();
+const userdb={emriIPerdoruesit:"FilaniIFistekut", emri: "Filan", mbiemri:"Fisteku", email:"fs@gmail.com", password:"libraria12345", bio:"Pershendetje, te gjitheve", img:"img/rr.jpg"}
 
 router.get("/",(req, res)=>{
-    res.render("includes/navBar",{name: "Hamit"});
+    res.render("profili/index",{user: userdb});
 
 });
 module.exports=router;
