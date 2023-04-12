65b2ba26cc4054e244368ef45193464458d53fef
dardangjoka <dardan.gjoka@gmail.com>
2023-04-12 22:44:51 +0200

    Loging in functionality

--- controllers/bookprofile.js
+++ controllers/bookprofile.js
@@ -5,6 +5,7 @@ const dbBooks=[ {id:1, titulli:"Beni ecen nuk vetem", autori:"Fan Noli", img:"/i
                 {id:4, titulli:"Beni nuk ecen vetem", autori:"Fan Noli", img:"/img/img1.jpg", zhanri:"shkence"},
                 {id:5, titulli:"Beni nuk eshte fukara", autori:"Fan Noli", img:"/img/img1.jpg", zhanri:"romantik"},
                 {id:6, titulli:"Beni ecen vetem", autori:"Fan Noli", img:"/img/img1.jpg", zhanri:"romantik"},
+                {id:6, titulli:"Beni ecen vetem", autori:"Fan Noli", img:"/img/img1.jpg", zhanri:"romantik"},
             ];
 
 exports.getBookProfile=(req, res)=>{
@@ -15,14 +16,15 @@ exports.getBookProfile=(req, res)=>{
 
 exports.getAllBooks=(req, res)=>{
     const zhanri=req.query;
+    let zhanriAktiv="clear";
     if(!(Object.keys(zhanri).length === 0) && zhanri.zhanri!=="clear"){
-        console.log(zhanri)
+        zhanriAktiv=zhanri.zhanri;
         const filterdBooks=dbBooks.filter((c)=>{
             return zhanri.zhanri===c.zhanri;
         });
-        return res.render("librat/index",{titulliIfaqes:"Librat me zhaner:"+zhanri.zhanri, librat: filterdBooks});
+        return res.render("librat/index",{titulliIfaqes:"Librat me zhaner: " + zhanri.zhanri, active: zhanriAktiv, librat: filterdBooks});
     }
-    res.render("librat/index",{titulliIfaqes:"Te gjitha librat", librat: dbBooks})
+    res.render("librat/index",{titulliIfaqes:"Te gjitha librat", librat: dbBooks, active: zhanriAktiv})
 }
 
 //module.exports=dbBooks;
